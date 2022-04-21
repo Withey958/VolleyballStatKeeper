9e15ff362e08aeb9119704b902ae709c138269b5
lwitheyArete <withey89@gmail.com>
2022-04-21 21:22:19 +0100

    update team page

--- app/src/main/java/arete/arete/volleyballstatkeeper/ui/homescreen/ScreenHome.kt
+++ app/src/main/java/arete/arete/volleyballstatkeeper/ui/homescreen/ScreenHome.kt
@@ -1,5 +1,6 @@
 package arete.arete.volleyballstatkeeper.ui.homescreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,16 +36,89 @@ fun HomeScreen(
             }
         }
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize(),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Top
         ) {
             TeamSelector()
+            TeamRecord()
+            SelectionButtons()
         }
     }
 }
 
-@Preview
+@Composable
+fun SelectionButtons() {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .height(64.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .fillMaxSize()
+            .padding(4.dp),
+            onClick = { /*TODO*/ }) {
+            Text(text = "Team Players")
+        }
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+            .padding(4.dp),
+            onClick = { /*TODO*/ }) {
+            Text(text = "Games")
+        }
+    }
+}
+
+@Composable
+fun TeamRecord() {
+    // Games played
+    // Games won
+    // win percentage
+    // Kills
+    // Blocks
+    // Team members
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp)
+            .background(color = Color.DarkGray, shape = RoundedCornerShape(16.dp))
+            .padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TeamStat(statname = "Games Played")
+        TeamStat(statname = "Games Won")
+        TeamStat(statname = "Win Percentage")
+        TeamStat(statname = "Kills")
+        TeamStat(statname = "Blocks")
+        TeamStat(statname = "Team Members")
+    }
+
+
+}
+
+@Composable
+fun TeamStat(statname: String) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = statname,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = Color.White
+        )
+        Text(text = "0", fontWeight = FontWeight.ExtraBold, color = Color.White)
+    }
+}
+
 @Composable
 fun TeamSelector() {
     Column() {
@@ -53,7 +126,7 @@ fun TeamSelector() {
             modifier = Modifier.padding(8.dp),
             text = "Team",
             fontWeight = FontWeight.Bold,
-            color = Color.White,
+            color = Color.Black,
             fontSize = 26.sp,
         )
         LazyRow(
